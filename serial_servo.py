5abeccc4f2c699bbb0fd3b0502da88bcd4dee9b6
johnmrushing <johnmrushing@gmail.com>
2019-03-26 23:49:56 -0400

    create serial_servo.py
    Reads integer from user and sends to ttyS0@9600 with checksum

---
+++ serial_servo.py
@@ -0,0 +1,11 @@
+import serial
+import array
+ser = serial.Serial(port='/dev/ttyS0',baudrate=9600)
+print("Connected to: " + ser.portstr)
+while True:
+	input = raw_input("Enter angle for servo: ")
+	direction = 1 if int(input) >= 0 else 0
+	mag1 = abs(int(input))/256
+	mag2 = abs(int(input))%256
+	checksum = (direction + mag1 + mag2)%255
+	ser.write(str( chr(0xFF) + chr(0xFF) + chr(direction) + chr(mag1) + chr(mag2) + chr(checksum)))
