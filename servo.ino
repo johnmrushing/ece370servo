30b8cd77da57bcbf3321f82b31ba482f5f46ef9f
johnmrushing <johnmrushing@gmail.com>
2019-03-27 09:56:13 -0400

    update servo.ino
    updated all comments; updated formatting so it's consistent; removed extraneous global variables

--- servo.ino
+++ servo.ino
@@ -6,14 +6,19 @@
 
 int t1, t2;
 float thetaDesired = 0, thetaCurrent, thetaChange, thetaError;
-int tickCount, ticksDesired;
+int tickCount;
 int outerIRRead, innerIRRead;
 
 float kp = 4; // allows for an error of about 10 degrees maximum
 
 char bytes[6];
 
-void setVelocity(float velocity) {
+void setVelocity(float velocity)
+{
+  /*
+   * Updates velocity 0-127-254. Old, proven code from previous homework.
+   */
+  
   if (velocity >= 127) {
     analogWrite(BACKWARD, 0);
     analogWrite(FORWARD, (velocity - 127) * 2);
@@ -24,89 +29,128 @@ void setVelocity(float velocity) {
   }
 }
 
-float getAngle(int count){
-  return count * (90 / 75.8); //1.2 degrees per tick
+float getAngle(int count)
+{
+  /*
+   * converts from number of ticks observed to a corresponding angle
+   */
+  
+  return count * (90 / 75.8); // 1.18 degrees per tick
 }
 
-float PIDControl(float desiredtheta, float theta){
-  return kp * (desiredtheta - theta);
+float PIDControl(float desired, float theta)
+{
+  /*
+   * returns proportional motor control based on error observed
+   * between angle desired and current angle
+   */
+  
+  return kp * (desired - theta);
 }
 
-void tickInterrupt(){
+void tickInterrupt()
+{
+  /*
+   * reads each IR sensor and compares them
+   * based on my quadrature encoder:
+   *   SAME = forward
+   *   DIFF = backward
+   */
 
   outerIRRead = digitalRead(OUTERIR);
   innerIRRead = digitalRead(INNERIR);
   
   if(outerIRRead != innerIRRead)
-    tickCount++;
+    tickCount++; // forward
   else
-    tickCount--; 
+    tickCount--; // backward
 }
 
 void timeUpdate()
 {
+  /*
+   * Allows us to update our motor control value every 20 milliseconds (50 Hz)
+   * This is done so we 1) don't update uselessly and
+   * 2) so we still update even when the wheel is moving slowly
+   */
+  
   t1 = millis();
-  if(t1 - t2 >= 20){
+  
+  if(t1 - t2 >= 20)
+  {
     setVelocity(PIDControl(thetaDesired, thetaCurrent) + 127);
     t2 = t1;
   }
 }
 
-void setup() {
-
+void setup()
+{
+  /*
+   * Establishes two serial connections
+   * Sets up motor pins
+   * Sets up IR sensor pins
+   * Sets up interrupt
+   * Resets current angle to 0
+   */
+  
   Serial.begin(9600); // arduino IDE
   Serial1.begin(9600); // pi UART
   
-  pinMode(FORWARD, OUTPUT);
-  pinMode(BACKWARD, OUTPUT);
+  pinMode(FORWARD, OUTPUT); // motor pin
+  pinMode(BACKWARD, OUTPUT); // motor pin
   
-  pinMode(OUTERIR, INPUT_PULLUP);
-  pinMode(INNERIR, INPUT_PULLUP);
+  pinMode(OUTERIR, INPUT_PULLUP); // IR sensor doing most of the work
+  pinMode(INNERIR, INPUT_PULLUP); // IR sensor for comparison
   
-  attachInterrupt(digitalPinToInterrupt(OUTERIR), tickInterrupt, CHANGE);
+  attachInterrupt(digitalPinToInterrupt(OUTERIR), tickInterrupt, CHANGE); // four interrupts per spin
   
   thetaCurrent = 0; // reset so whatever angle is current is 0
   
   t1 = millis(); // start timer
 }
 
-void loop() {
-
-  /*
+void loop()
+{
+  /* Primarily, this checks for serial data
    * Six bytes from pi UART (Serial1)
    * (0xFF) (0xFF) (direction) (magnitude/256) (magnitude%256) (checksum)
+   * 
+   * Then, it updates the motor speed if it's time to do it
+   * using the PID controller
    */
 
-  while(true){
+  while(true)
+  {
     
-    if(Serial1.available() >= 6){
+    if(Serial1.available() >= 6)
+    {
   
-      for ( char i = 0; i < 7; i++ )
-      {
+      for ( char i = 0; i < 7; i++ ) // read six bytes
         bytes[i] = Serial1.read();
-      }
       
-      if ( bytes[0] == char(0xFF) && bytes[1] == char(0xFF) )
+      if ( bytes[0] == char(0xFF) && bytes[1] == char(0xFF) ) // check header
       {
-        char checksum = (bytes[2] + bytes[3] + bytes[4])%256;
-        if ( checksum == bytes[5] )
+        char checksum = (bytes[2] + bytes[3] + bytes[4])%256; // same checksum logic as the python script
+        if ( checksum == bytes[5] ) // if the checksum passes
         {
-          thetaDesired = (bytes[3] * 256) + (unsigned char)bytes[4];
-          if ( 1 == bytes[2] )
-            thetaDesired *= -1;
-          Serial.println(thetaDesired);
+          thetaDesired = (bytes[3] * 256) + (unsigned char)bytes[4]; // updated: read unsigned so numbers between 127 and 255 work
+          if ( 1 == bytes[2] ) 
+            thetaDesired *= -1; // I recognize this is opposite of what's intuitive, but I made my encoder backwards
+          Serial.println(thetaDesired); // this is mostly for debugging; sends to arduino IDE
         }
+
+        
         else // bad checksum
-        {
           Serial.write("Error in checksum.");
-        }
       }
       else // bad header
-      {
         Serial.write("Error in header.");
-      }
       
     }
+
+    /*
+     * Spec is to operate +/- 720 deg
+     */
   
     if(thetaDesired > 720)
       thetaDesired = 720;
